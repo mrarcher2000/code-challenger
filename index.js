1b67cba6de732e0d538cc82059fafd5372b362d2
EliptickBlock5 <nbjohnson@gmail.com>
2021-06-28 16:06:51 -0700

    connecting to mongo db, fetching posts dummy text from mongo

--- index.js
+++ index.js
@@ -2,19 +2,33 @@ const { ApolloServer } = require('apollo-server');
 const gql = require('graphql-tag');
 const mongoose = require('mongoose');
 
+const Post = require('./models/Post');
 const { MONGODB } = require('./config');
 
 const typeDefs = gql`
+    type Post{
+        id: ID!
+        body: String!
+        createdAt: String!
+        username: String!
+    }
     type Query{
-        sayHi: String!
+        getPosts: [Post]
     }
 `
 
 const resolvers = {
     Query: {
-        sayHi: () => 'Hello test text'
+       async getPosts(){
+            try{
+                const posts = await Post.find();
+                return posts;
+            } catch(err) {
+                throw new Error(err);
+            }
         }
-}
+    }
+};
 
 const server = new ApolloServer({
     typeDefs,
