66bf2421f2da60cdcc04b006737441ba04ab2852
EliptickBlock5 <nbjohnson@gmail.com>
2021-06-28 20:44:40 -0700

    mutation working in graphql

--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -1,43 +1,47 @@
-const bycrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
+const bycrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
-const { SECRET_KEY } = require('../../config');
-const User = require('../../models/User');
-
+const { SECRET_KEY } = require("../../config");
+const User = require("../../models/User");
 
 module.exports = {
-    Mutation: {
-        async register(_,{ registerInput : { username, email, password, confirmPassword }, context, info})
-            {
-            // TODO validate data of user
-            // TODO ensure user doens't alrady exist
-            // TODO hash passw and creat auth token
-            password = await bycrypt.hash(password, 11);
+  Mutation: {
+    async register(
+      _,
+      { registerInput: { username, email, password, confirmPassword } },
+      content,
+      info
+    ) {
+      // TODO validate data of user
+      // TODO ensure user doens't alrady exist
+      // TODO hash passw and creat auth token
+      password = await bycrypt.hash(password, 11);
 
-            const newUser = new User({
-                email,
-                username,
-                password,
-                createdAt: new Date().toISOString()
+      const newUser = new User({
+        email,
+        username,
+        password,
+        createdAt: new Date().toISOString()
+      });
 
-            });
+      const res = await newUser.save();
 
-            const res = await newUser.save();
+      const token = jwt.sign(
+        {
+          id: res.id,
+          email: res.email,
+          username: res.username,
+        },
+        SECRET_KEY,
+        { expiresIn: "1h" }
+      );
 
-            const token = jwt.sign({
-                id: res.id,
-                email: res.email,
-                username: res.username
-            }, SECRET_KEY, { expiresIn: '1h'});
-
-            //spread function return
-            return {
-                ...res._doc,
-                id: res._id,
-                token
-            }
-
-
-        }
+      //spread function return
+      return {
+        ...res._doc,
+        id: res._id,
+        token
+      };
     }
-}
+  }
+};
