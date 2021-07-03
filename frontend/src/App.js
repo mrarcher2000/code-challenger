caa7ec5a4b319e94369824d9a9e69b0cf3927f79
unknown <archernich@gmail.com>
2021-07-02 23:33:17 -0700

    update to editor styling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Nav from './components/Nav'
+import Editor from './components/Editor'
 import './index.css'
 
 function App() {
   return (
     <div className="App">
       <Nav></Nav>
+      <Editor mode={'javascript'}></Editor>
     </div>
   );
 }
--- frontend/src/components/Editor.js
+++ frontend/src/components/Editor.js
@@ -1,45 +1,74 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import AceEditor from "react-ace";
+import Terminal from "terminal-in-react";
 import IFrame from './IFrame';
 
 
 import "ace-builds/src-noconflict/mode-html";
+import "ace-builds/src-noconflict/mode-javascript";
 import "ace-builds/src-noconflict/theme-cobalt";
 
-function Editor() {
+function Editor(props) {
 
-    const [iframeCode, setiFrameCode] = useState('');
+    const { mode } = props;
+
+    const [iframeCode, setiFrameCode] = useState(``);
+    const [editorMode, setEditorMode] = useState(props);
 
     let iframeValue;
 
-    const onChange = (newValue) =>  {
+    const onChange = (newValue) => {
+        var iframeValue = newValue;
+        setiFrameCode(`<br /> ${newValue}`);
 
-        var  iframeValue = newValue;
-        setiFrameCode(newValue);
-        console.log(newValue);
-    } 
+        // console.log(newValue);
+        // console.log(mode);
+    }
 
+
+    const handleClick = (e) => {
+        e.preventDefault();
+        const runCode = new Function(iframeCode)();
+    }
 
     return (
         // <div>
         //     <iframe width="100%" height="300" src="//jsfiddle.net/codeeditorNAT/gcp8vyks/2/embedded/" allowfullscreen="allowfullscreen" allowpaymentrequest frameborder="0"></iframe>
         // </div>
-        <div id="codeEditor">
-            <AceEditor
-            className="code"
-            mode="html"
-            theme="cobalt"
-            onChange={onChange}
-            name="javascript_editor"
-            editorProps={{ $blockScrolling: true }}
-            />
+        <div style={{style: "flex"}}>
+            <div className="codeEditor">
+                <div>
+                    <AceEditor
+                        height="20%"
+                        width="200%"
+                        className="code"
+                        mode={mode}
+                        theme="cobalt"
+                        onChange={onChange}
+                        name="javascript_editor"
+                        editorProps={{ $blockScrolling: true }}
+                    />
 
+                    <button onClick={handleClick} style={{ color: 'white', backgroundColor: 'blue', border: 'none', position: 'absolute', }}>Run Code</button>
 
-            <IFrame className="frame" srcDoc={iframeCode}></IFrame>
-            {/* <iframe width="100%" height="300" src={iframeCode}></iframe> */}
+                    <IFrame style={{ width: '200%', height: '20%', }} className="frame" srcDoc={iframeCode}></IFrame>
+                    <div>
+                        <Terminal
+                            color='white'
+                            backgroundColor='black'
+                            barcolor='black'
+                            style={{ fontWeight: "bold", fontSize: '1em', height: '15%', width: '200%', minHeight: '0px' }}
+                            className='terminal-base sc-bdVaJa gSZAyM'
+                            id="terminal"
+                            startState='minimised open'
+                            watchConsoleLogging
+                        />
+                    </div>
+                </div>
+            </div>
         </div>
 
-        
+
     )
 }
 
