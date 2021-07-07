8b4009e3d54e81b4418e5965ccedc00295829db1
ThomasKubant <77358241+ThomasKubant@users.noreply.github.com>
2021-07-07 16:35:23 -0700

    fixed server

--- index.js
+++ index.js
@@ -15,6 +15,10 @@ const server = new ApolloServer({
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/build')));
   }
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './frontend/build/index.html'));
+  });
 mongoose
     .connect(MONGODB, { useNewUrlParser: true })
     .then(() => {
